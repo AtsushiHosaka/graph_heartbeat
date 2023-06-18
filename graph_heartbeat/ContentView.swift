9c8c829b7125297017de251594502926cb82d3ec
AtsushiHosaka <hosaka2917@gmail.com>
2023-06-19 00:39:59 +0900

    Initial Commit

---
+++ graph_heartbeat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  graph_heartbeat
+//
+//  Created by 保坂篤志 on 2023/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ graph_heartbeat/graph_heartbeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  graph_heartbeatApp.swift
+//  graph_heartbeat
+//
+//  Created by 保坂篤志 on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct graph_heartbeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
